6d8d3c35562986ef7b0c788377b5b428a73a87e5
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-10 19:44:59 +0600

    Setup Database

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+
+
+// database connection
+const connectDB = require('./db/connect')
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start();
