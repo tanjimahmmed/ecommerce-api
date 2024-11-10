9e9368ddd548a97fcb176eae91524db8890ee1a8
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-10 21:22:14 +0600

    add route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors')
 
 const express = require('express');
 const app = express();
@@ -7,6 +8,19 @@ const app = express();
 
 // database connection
 const connectDB = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000;
 
