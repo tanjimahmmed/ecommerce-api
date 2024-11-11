4aebdef414efee1bd66a9e40786413a1352bdded
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-11 19:24:22 +0600

    signed secure flags

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,7 +15,9 @@ const attachCookiesToResponse = ({res, user}) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        expires: new Date(Date.now() + oneDay)
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        signed: true
     })
 
     
