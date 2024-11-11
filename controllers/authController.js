78d075e402a7a44b4d33926951aa043eef4f5a5e
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-11 18:24:19 +0600

    refactor JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const customError = require('../errors');
-const jwt = require('jsonwebtoken')
+const {createJWT} = require('../utils')
 
 const register = async (req, res) => {
     const {email, name, password} = req.body;
@@ -19,7 +19,9 @@ const register = async (req, res) => {
 
     const user = await User.create({name, email, password, role});
     const tokenUser = {name: user.name, userId: user._id, role: user.role}
-    const token = jwt.sign(tokenUser, 'jwtSecret', {expiresIn: '1d'})
+    const token = createJWT({payload: tokenUser})
+
+
     res.status(StatusCodes.CREATED).json({user:tokenUser, token})
 }
 
