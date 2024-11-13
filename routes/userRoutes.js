301ff468c459410318dbd0d0d000e6961ddf8856
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-13 12:18:12 +0600

    add show me current user or admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ const {
 
 router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers)
 
-router.route('/showMe').get(showCurrentUser)
+router.route('/showMe').get(authenticateUser, showCurrentUser)
 router.route('/updateUser').patch(updateUser)
 router.route('/updateUserPassword').patch(updateUserPassword)
 
