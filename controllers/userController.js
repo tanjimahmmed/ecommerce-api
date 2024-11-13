d4eaf6f8f068a662056a4e4a14aaea0e3de880e2
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-13 17:33:55 +0600

    update user controller with user.save()

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const {createTokenUser, attachCookiesToResponse} = require('../utils');
 
 
 const getAllUsers = async (req, res) => {
@@ -22,8 +23,41 @@ const showCurrentUser = async (req, res) => {
     res.status(StatusCodes.OK).json({user: req.user})
 }
 
+// update user with findOneAndUpdate
+// const updateUser = async (req, res) => {
+//     const {email, name} = req.body;
+//     if(!email || !name) {
+//         throw new CustomError.BadRequestError('Please provide both values')
+//     }
+
+//     const user = await User.findOneAndUpdate(
+//         {_id: req.user.userId},
+//         {email, name},
+//         {new: true, runValidators: true}
+//     )
+
+//     const tokenUser = createTokenUser(user)
+//     attachCookiesToResponse({res, user: tokenUser})
+//     res.status(StatusCodes.OK).json({user: tokenUser})
+// }
+
+// update user with user.save()
 const updateUser = async (req, res) => {
-    res.send(req.body)
+    const {email, name} = req.body;
+    if(!email || !name) {
+        throw new CustomError.BadRequestError('Please provide both values')
+    }
+
+    const user = await User.findOne({_id: req.user.userId})
+
+    user.email = email;
+    user.name = name;
+
+    await user.save();
+
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({res, user: tokenUser})
+    res.status(StatusCodes.OK).json({user: tokenUser})
 }
 
 const updateUserPassword = async (req, res) => {
